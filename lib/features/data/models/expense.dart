8c3dc407425dc4cbc5cb273312ce1daa6c01eb38
Ayush Umrao <umraoayush@gmail.com>
2026-05-13 14:26:01 +0530

    Added Expense Model

---
+++ lib/features/data/models/expense.dart
@@ -0,0 +1,20 @@
+import 'package:isar/isar.dart';
+
+part 'expense.g.dart';
+
+@collection
+class Expense {
+  Id id = Isar.autoIncrement;
+
+  @Index() 
+  DateTime? date;
+
+  double? amount;
+
+  
+  @Index(type: IndexType.value)
+  String? category;
+
+  
+  Expense({this.date, this.amount, this.category});
+}
