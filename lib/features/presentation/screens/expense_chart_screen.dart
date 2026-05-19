92b3cefc8af06bb01a4fb0582fbc89ef7e320c7d
Ayush Umrao <umraoayush@gmail.com>
2026-05-19 15:54:34 +0530

    Add navigation to ExpenseChartScreen and Added Charts To Show Expense

--- lib/features/presentation/screens/expense_chart_screen.dart
+++ lib/features/presentation/screens/expense_chart_screen.dart
@@ -1,70 +1,89 @@
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:graphify/features/data/models/expense.dart';
+import 'package:graphify/features/presentation/providers/expenses.dart';
 
-class ExpenseChartScreen extends StatefulWidget {
+class ExpenseChartScreen extends ConsumerStatefulWidget {
   const ExpenseChartScreen({super.key});
 
   @override
-  State<ExpenseChartScreen> createState() => _ExpenseChartScreenState();
+  ConsumerState<ExpenseChartScreen> createState() => _ExpenseChartScreenState();
 }
 
-class _ExpenseChartScreenState extends State<ExpenseChartScreen> {
+class _ExpenseChartScreenState extends ConsumerState<ExpenseChartScreen> {
   @override
   Widget build(BuildContext context) {
+    final expenses = ref.watch(expensesProvider).value ?? [];
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Charts Screen'),
         backgroundColor: Colors.lightGreen,
       ),
-      body: Card(
-        child: BarChart(
-          BarChartData(
-            barGroups: _barChartGroupData(),
-            backgroundColor: Colors.white,
-            borderData: _flBorderData(),
-            gridData: _flGridData(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            
+            height: 500,
+            width: 400,
+            child: Card(
+              child: BarChart(
+                BarChartData(
+                  
+                  barGroups: buildChartData(expenses),
+                  barTouchData: BarTouchData(
+                    enabled: true,
+            
+                    touchTooltipData: BarTouchTooltipData(
+                      getTooltipItem: (group, groupIndex, rod, rodIndex) {
+                        final expense = expenses[group.x];
+            
+                        return BarTooltipItem(
+                          '${expense.category}\n'
+                          '${expense.amount}',
+                          const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  
+                ),
+                
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
 }
 
-FlBorderData _flBorderData() {
-  return FlBorderData(
-    show: true,
-    border: Border(top: BorderSide(color: Colors.black, width: 3)),
-  );
-}
 
-FlGridData _flGridData() {
-  return FlGridData(
-    drawHorizontalLine: true,
-    show: true,
-    horizontalInterval: 10,
-    verticalInterval: 10,
-    drawVerticalLine: false,
-    checkToShowHorizontalLine: (value) => false,
-  );
-}
+List<BarChartGroupData> buildChartData(
+  List<Expense> expenses,
+) {
+  return expenses.asMap().entries.map((entry) {
 
-List<BarChartGroupData> _barChartGroupData() {
-  return [
-    BarChartGroupData(
-      x: 1,
-      barRods: [BarChartRodData(toY: 8, color: Colors.red)],
-    ),
-    BarChartGroupData(
-      x: 2,
-      barRods: [BarChartRodData(toY: 12, color: Colors.green)],
-    ),
-    BarChartGroupData(
-      x: 3,
-      barRods: [BarChartRodData(toY: 5, color: Colors.blue)],
-    ),
-    BarChartGroupData(
-      x: 4,
-      barRods: [BarChartRodData(toY: 15, color: Colors.orange)],
-    ),
-  ];
+    final index = entry.key;
+    final expense = entry.value;
+
+    return BarChartGroupData(
+      x: index,
+
+      showingTooltipIndicators: [0],
+
+      barRods: [
+        BarChartRodData(
+          toY: expense.amount!,
+          width: 20,
+          borderRadius: BorderRadius.circular(4),
+        ),
+      ],
+    );
+  }).toList();
 }
