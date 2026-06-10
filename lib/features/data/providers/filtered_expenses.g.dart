50349093ae425dac6c0054860cd77b8c571af91c
Ayush <umraoayush5@gmail.com>
2026-06-10 13:57:56 +0530

    Implement expense filtering and add BottomToolBar
    * Add date picker widget and integrate it into expense form
    
    * Add Custom category selection feature to expense home screen
    
    * Add monthly expense summary card to expenses view screen
    
    * Add expense pie chart and category totals provider for enhanced analytics and beautify UI
    
    * Implemented Dark Mode and theme management
    
    * Add theme customization for app bar, input decoration, and buttons; enhance UI consistency
    
    * add dark mode theme switching with persistent storage
    
    * Implement expense filtering and add BottomToolBar
    
    ---------
    
    Co-authored-by: Ayush Umrao <umraoayush@gmail.com>

--- lib/features/data/providers/filtered_expenses.g.dart
+++ lib/features/data/providers/filtered_expenses.g.dart
@@ -1,26 +1,26 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'theme_provider.dart';
+part of 'filtered_expenses.dart';
 
 // **************************************************************************
 // RiverpodGenerator
 // **************************************************************************
 
-String _$themeNotifierHash() => r'f72329e28c7539df035a2dfe0b1e33b2aceae3e0';
+String _$filteredExpensesHash() => r'6bcf615cc617424b7b0ab2c8ac45dbfc3db3d4ea';
 
-/// See also [ThemeNotifier].
-@ProviderFor(ThemeNotifier)
-final themeNotifierProvider =
-    AutoDisposeNotifierProvider<ThemeNotifier, ThemeMode>.internal(
-  ThemeNotifier.new,
-  name: r'themeNotifierProvider',
+/// See also [filteredExpenses].
+@ProviderFor(filteredExpenses)
+final filteredExpensesProvider =
+    AutoDisposeProvider<AsyncValue<List<Expense>>>.internal(
+  filteredExpenses,
+  name: r'filteredExpensesProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
       ? null
-      : _$themeNotifierHash,
+      : _$filteredExpensesHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
-typedef _$ThemeNotifier = AutoDisposeNotifier<ThemeMode>;
+typedef FilteredExpensesRef = AutoDisposeProviderRef<AsyncValue<List<Expense>>>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
